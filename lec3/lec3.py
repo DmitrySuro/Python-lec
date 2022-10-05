01189d502117845c2ccab3f562526718d093f531
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-10-05 21:38:12 +0300

    add lec3

--- lec3/lec3.py
+++ lec3/lec3.py
@@ -1,18 +1,43 @@
-path = 'C:\Users\Пользователь\Desktop\Python\lec3\file.txt'
-f = open(path, 'r')
-data = f.read() + ' '
-f.close()
+
+# path = 'C:\Users\Пользователь\Desktop\Python\lec3\file.txt'
+# f = open('file.txt', 'r')
+# data = f.read() + ' '
+# f.close()
 
 
-numbers = []
+# numbers = []
 
-while data != '':
-    space_pos = data.index(' ')
-    numbers.append(int(data[:space_pos]))
+# while data != '':
+#     space_pos = data.index(' ')
+#     numbers.append(int(data[:space_pos]))
 
-out = []
+# out = []
 
-for e in numbers:
-    if not e % 2:
-        out.append((e, e ** 2))
-print(out)
+# for e in numbers:
+#     if not e % 2:
+#         out.append((e, e ** 2))
+# print(out)
+
+
+data = '1 2 3 5 8 15 38'.split()
+res = map(int, data)
+res = filter(lambda x: not x % 2, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
+
+
+# li = [x for x in range(1,21)]
+
+# li = list(map(lambda x:x+10,li))
+
+# print(li)
+
+# data = list(map(int,'1 2 3 4'.split()))
+
+# for e in data:
+#     print(e)
+
+# print('--')
+
+# for e in data:
+#     print(e)
